91c19ed2fb8e37d3c7cb2df0137d12c49e4da2cc
Владислав Горохов <gbg.0101@mail.ru>
2019-03-29 13:19:13 +0600

    Сделаны заготовки для связи с маршрутами!!!!!!

--- TravelWay/Controllers/HomeController.cs
+++ TravelWay/Controllers/HomeController.cs
@@ -16,17 +16,30 @@ namespace TravelWay.Controllers
         }
 
         [HttpPost]
-        public ActionResult Routes(Searcher searcher)
+        public ActionResult Routes(Searcher searcherOriginal)
         {
+            Searcher searcher = searcherOriginal;
             ViewBag.isFound = false;
             var date = DateTime.Now;
             
-            if (searcher.From is null || searcher.To is null || searcher.DepartureDate.Date < date.Date)
+            if (searcherOriginal.From is null || searcherOriginal.To is null || searcherOriginal.DepartureDate.Date < date.Date)
             {
+
                 return View("/Views/Home/Index.cshtml");
             }
             else
             {
+                searcher.From.ToLower();
+                switch (searcher.From)
+                {
+
+                    case "астана":
+                        searcher.From = "TSE";
+                        break;
+                }
+                TicketsParser ticketsParser = new TicketsParser(URL);
+                ViewBag.Tickets = ticketsParser.GetTickets();
+                ViewBag.SearcherOriginal = searcherOriginal;
                 ViewBag.Searcher = searcher;
                 return View();
             }
